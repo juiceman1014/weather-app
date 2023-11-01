67a9858ef3a8be9bf1d06e5c2478e41c2035bb59
juiceman1014 <justintran1014@gmail.com>
2023-11-01 15:33:00 -0700

    getWeather will now also provide city name, region, county, and the current temperature

--- javascript.js
+++ javascript.js
@@ -1,8 +1,15 @@
+let userCity;
+
+
 async function getWeather(){
     const response = await fetch("https://api.weatherapi.com/v1/current.json?key=cd06bddc6518484ba1721122232810&q=san_jose",
     {mode: "cors"});
     const cityData = await response.json();
+    console.log(cityData.location.name);
+    console.log(cityData.location.region);
+    console.log(cityData.location.country);
     console.log(cityData.current.temp_f);
+    
 }
 
 getWeather();
