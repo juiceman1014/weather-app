8576989beefc71b0f1ac8235cf6f336eadc3454b
juiceman1014 <justintran1014@gmail.com>
2023-11-03 23:16:55 -0700

    default city is now san jose

--- javascript.js
+++ javascript.js
@@ -1,8 +1,17 @@
-let userCity;
+const defaultCity = "san jose";
+let userCity = defaultCity;
 
+const searchBar = document.getElementById("search-bar");
+
+searchBar.addEventListener("keydown", (event) =>{
+    if(event.key == "Enter"){
+        userCity = searchBar.ariaValueMax;
+        fetchData(userCity);
+    }
+})
 
 async function getWeather(){
-    const response = await fetch("https://api.weatherapi.com/v1/current.json?key=cd06bddc6518484ba1721122232810&q=san_jose",
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=cd06bddc6518484ba1721122232810&q=${userCity}`,
     {mode: "cors"});
     const cityData = await response.json();
     console.log(cityData.location.name);
