5ec89fb6d3742b961ef5f0745896127ef5a60caf
juiceman1014 <justintran1014@gmail.com>
2023-11-10 18:39:20 -0800

    information now populates the display upon entering your city

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
+const cityName = document.getElementById("city");
+const regionName = document.getElementById("region");;
+const countryName = document.getElementById("country");;
+const tempLevel = document.getElementById("temperature");;
+
 const defaultCity = "san jose";
 let userCity = defaultCity;
 
@@ -7,18 +12,26 @@ searchBar.addEventListener("keydown", (event) =>{
     if(event.key == "Enter"){
         userCity = searchBar.value;
         getWeather(userCity);
+        cityName.textContent = "City: ";
+        regionName.textContent = "Region: ";
+        countryName.textContent = "Country: ";
+        tempLevel.textContent = "Temperature (f): ";
     }
+
 })
 
 async function getWeather(searchTerm){
-    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=cd06bddc6518484ba1721122232810&q=${userCity}`,
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=cd06bddc6518484ba1721122232810&q=${searchTerm}`,
     {mode: "cors"});
     const cityData = await response.json();
     console.log(cityData.location.name);
     console.log(cityData.location.region);
     console.log(cityData.location.country);
     console.log(cityData.current.temp_f);
-    
+    cityName.textContent += `${cityData.location.name}`;
+    regionName.textContent += `${cityData.location.region}`;
+    countryName.textContent += `${cityData.location.country}`;
+    tempLevel.textContent += `${cityData.current.temp_f}`;
 }
 
-getWeather(defaultCity);
+getWeather(userCity);
