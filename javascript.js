a55938cf7ec2ac5cebe9da60ac9cfd075918c91b
juiceman1014 <justintran1014@gmail.com>
2023-11-10 18:43:06 -0800

    added a button, no functionality yet, that can toggle the temp between F/C

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
 const cityName = document.getElementById("city");
-const regionName = document.getElementById("region");;
-const countryName = document.getElementById("country");;
-const tempLevel = document.getElementById("temperature");;
+const regionName = document.getElementById("region");
+const countryName = document.getElementById("country");
+const tempLevel = document.getElementById("temperature");
+const tempToggle = document.getElementByiId("temp-toggle")
 
 const defaultCity = "san jose";
 let userCity = defaultCity;
@@ -17,17 +18,12 @@ searchBar.addEventListener("keydown", (event) =>{
         countryName.textContent = "Country: ";
         tempLevel.textContent = "Temperature (f): ";
     }
-
 })
 
 async function getWeather(searchTerm){
     const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=cd06bddc6518484ba1721122232810&q=${searchTerm}`,
     {mode: "cors"});
     const cityData = await response.json();
-    console.log(cityData.location.name);
-    console.log(cityData.location.region);
-    console.log(cityData.location.country);
-    console.log(cityData.current.temp_f);
     cityName.textContent += `${cityData.location.name}`;
     regionName.textContent += `${cityData.location.region}`;
     countryName.textContent += `${cityData.location.country}`;
